b8496d6371876f39277c904cbb28253d819ae6de
sharosoo <shiningmoon@snu.ac.kr>
2023-01-19 14:37:37 +0900

    [fix] package name

--- python/kserve/setup.py
+++ python/kserve/setup.py
@@ -32,7 +32,7 @@ with open(pathlib.Path(__file__).parent.parent / 'VERSION') as version_file:
     version = version_file.read().strip()
 
 setuptools.setup(
-    name='kserve',
+    name='kserve-mathking',
     version=version,
     author="The KServe Authors",
     author_email='ellisbigelow@google.com, hejinchi@cn.ibm.com, dsun20@bloomberg.net',
